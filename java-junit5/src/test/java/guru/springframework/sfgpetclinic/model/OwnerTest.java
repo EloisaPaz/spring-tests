e45772f1a79eb660b2b9e83b3e85423c88954da4
Eloisa Paz <eloisapaz28@gmail.com>
2020-02-09 22:07:26 -0300

    Add param value source exemple

--- java-junit5/src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ java-junit5/src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -2,18 +2,22 @@ package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OwnerTest implements ModelTest {
 
     @Test
     void dependentAssertions() {
-        Owner owner = new Owner(1L, "Joe", "Buck");
+
+        Owner owner = new Owner(1l, "Joe", "Buck");
         owner.setCity("Key West");
-        owner.setTelephone("998522620");
+        owner.setTelephone("1231231234");
 
         assertAll("Properties Test",
                 () -> assertAll("Person Properties",
@@ -21,9 +25,15 @@ class OwnerTest implements ModelTest {
                         () -> assertEquals("Buck", owner.getLastName())),
                 () -> assertAll("Owner Properties",
                         () -> assertEquals("Key West", owner.getCity(), "City Did Not Match"),
-                        () -> assertEquals("998522620", owner.getTelephone())
+                        () -> assertEquals("1231231234", owner.getTelephone())
                 ));
 
         assertThat(owner.getCity(), is("Key West"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Spring", "Framework", "Lolla"})
+    void testValueSource(String val) {
+        System.out.println(val);
     }
 }
